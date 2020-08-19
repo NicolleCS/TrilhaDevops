cc2cc22ce71a76029440d13525349a2ed9bd78e6
NicolleCS <nick.cruz123@hotmail.com>
2020-08-19 16:59:52 -0300

    Exercicios do curso de Python

--- Python - Exercicios/Jogos/advinhacao.py
+++ Python - Exercicios/Jogos/advinhacao.py
@@ -1,28 +1,68 @@
+import random
+
+import false as false
+
 print(
     '********************************\n'
     'Bem vindo ao jogo de advinhação!\n'
     '********************************\n'
 )
 
-numero_secreto = 42
+numero_random = random.randrange(101)
+numero_random_arredondado = round(numero_random)
+total_de_tentativas = 5
+escolheu = false
+pontos = 1000
 
-chute = input('Digite o seu número:')
+while(escolheu == false):
+    print(
+        "Qual nível de difículade?\n"
+        "(1) Fácil (2) Médio (3) Difícil"
+    )
 
-acertou = int(chute) == numero_secreto
+    nivel = int(input("Define o nível:"))
 
-chute_maior = chute > numero_secreto
+    if(nivel == 1):
+        total_de_tentativas = 20
+        break
+    elif(nivel == 2):
+        total_de_tentativas = 10
+        break
+    elif(nivel == 3):
+        total_de_tentativas = 5
+        break
+    else:
+        print("Número inválido!")
 
-chute_menor = chute < numero_secreto
 
-print("Você digitou", chute)
+for rodada in range (1,total_de_tentativas +1) :
 
-if acertou:
-    print("Você acertoou!! Parabéns!")
-else:
-    if(chute_maior):
-        print("Você errou! O seu chute foi maior que o número secreto.")
-    elif(chute_menor):
-        print("Você errou! O seu chute foi menor que o número secreto.")
+    print("Tentativa {} de {}".format(rodada,total_de_tentativas))
+    chute = input('Digite o seu número entre 1 e 100:')
+    acertou = int(chute) == numero_random_arredondado
+    chute_maior = int(chute) > numero_random_arredondado
+    chute_menor = int(chute) < numero_random_arredondado
+
+    print("Você digitou", chute)
+
+    if (chute < 1 or chute > 100):
+        print("Você deve digitar um número entre 1 e 100!")
+        continue
+
+    if acertou:
+        print("Você acertoou!! Parabéns!")
+        break
+    else:
+        if(chute_maior):
+            print("Você errou! O seu chute foi maior que o número secreto.")
+        elif(chute_menor):
+            print("Você errou! O seu chute foi menor que o número secreto.")
+        pontos_perdidos = numero_random - chute
+        pontos = pontos - pontos_perdidos
+        print("Seus pontos são {}",pontos)
+
+    rodada += 1
+
 
 # numero1 = 10
 # numero2 = 10
